c3b9df0f56be6550aed8f8533f33157c2c48156d
padwalker <dawitgrmay@gmail.com>
2023-07-13 15:37:36 -0700

    print without using printf and put functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<string.h>
 /**
  * main - entry point
  *
  * Return: Always 1 (success)
  */
-void write(int, const char *, unsigned int);
-int main(void)
+int main(int argc, char *argv[])
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ;
+	write(2, message, strnlen(message,sizeof(message));
 	return (1);
 }
