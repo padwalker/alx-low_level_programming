9cbdf27084aeee53bdb256ee6e6dd37b4c755239
padwalker <dawitgrmay@gmail.com>
2023-07-13 15:46:13 -0700

    print without using printf and put functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ;
-	write(2, message, strnlen(message,sizeof(message)));
+	char message[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, message, strnlen(message, sizeof(message)));
 	return (1);
 }
