cf26da98b2ebb1949b19d2295278797d7ed65670
padwalker <dawitgrmay@gmail.com>
2023-07-13 15:24:22 -0700

    print without using printf and put functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 1 (success)
  */
-void write(int, const char *, unsigned int); 
+void write(1, const char *, 59); 
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
