ea6ffa4c66b1a9141cf180cbc1482ca295e55b01
padwalker <dawitgrmay@gmail.com>
2023-07-14 16:09:18 -0700

    upper and lower case alphabet putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main -entry piont
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* your code goes there */
+	int n = 'a';
+	int k = 'A';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	while (k <= 'Z')
+	{
+		putchar(k);
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
