6ad8559b65f1b62d38a627482a775ce5c5d1f6d1
padwalker <dawitgrmay@gmail.com>
2023-07-14 15:44:17 -0700

    print alphabet lower letter putchar()

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main -entry piont
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* your code goes there */
+	int n = 0;
+
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	while (n < 26)
+	{
+		putchar("%c", alpha[n]);
+		n++;
+	}
+	putchar("\n");
+	return (0);
+}
