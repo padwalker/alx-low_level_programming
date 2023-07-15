b6e2c176a435d946b0d37364dbe226a37f647702
padwalker <dawitgrmay@gmail.com>
2023-07-14 18:01:56 -0700

    print single digit combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - printing base 16 numbers
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* your code goes there */
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+		putchar(",");
+		putchar(" ");
+	}
+	putchar('\n');
+	return (0);
+}
