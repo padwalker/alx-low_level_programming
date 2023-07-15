285e09c423c2a64e7bc20f68574423fc5fe8a8f3
padwalker <dawitgrmay@gmail.com>
2023-07-14 17:50:20 -0700

    print base 16 numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /* more headers goes there */
 /**
- * main -entry piont
+ * main - printing base 16 numbers
  * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
 int main(void)
 {
 	/* your code goes there */
-	int n = 0;
+	int n;
 
-	for(n=0; n < 10; n++)
+	for (n = 0; n < 10; n++)
 		putchar('0' + n);
 	for (n = 'a'; n <= 'f'; n++)
-		putchar('0' + n);
+		putchar(n);
 
 	putchar('\n');
 	return (0);
