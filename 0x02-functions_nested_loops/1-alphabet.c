16d10e998a53cf21d9f8e8ce94cc2e22c3edc368
padwalker <dawitgrmay@gmail.com>
2023-07-18 10:27:52 -0700

    update on function definition

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,3 +9,13 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
+void print_alphabet()
+{
+	char *ch = "abcdefghijklmnopqrstuvwxyz\n";
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,13 +6,3 @@
  
 int _putchar(char c);
 void print_alphabet(void);
-
-void print_alphabet()
-{
-	char *ch = "abcdefghijklmnopqrstuvwxyz\n";
-	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
-}
