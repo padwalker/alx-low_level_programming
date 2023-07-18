ecc66fa57a9355d6768133b228ebd3a6411a839b
padwalker <dawitgrmay@gmail.com>
2023-07-18 22:56:39 +0000

    print last digit of a give number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit() - prints last digit using modulo operation
+ * @c - accepts one integer argument
+ *
+ * Return: Always 0 (success)
+ */ 
+int print_last_digit(int c)
+{
+	if (c == 0)
+	{
+		return (0);
+	}
+	if (c > 0 || c < 0)
+	{
+		return ( c % 10);
+	}
+	return (0);
+}
