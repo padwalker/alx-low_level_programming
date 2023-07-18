6354f80817b18d3c9bb8b209069a082bb23d4697
padwalker <dawitgrmay@gmail.com>
2023-07-18 20:21:08 +0000

    print alphabet x10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets in lower case 10 tims
+ * contains two for loops and uses two _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alph;
+
+	for(i = 0; i <= 9; i++)
+	{
+		for(alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+	}
+}
