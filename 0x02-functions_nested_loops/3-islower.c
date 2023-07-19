842b065a17f7aea4d16296bdeb48c4e46ea1defd
padwalker <dawitgrmay@gmail.com>
2023-07-19 00:19:49 +0000

    check for lowercase letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,6 +8,10 @@
  */
 int _islower(int c)
 {
+	if (lower >= 97 && lower <= 122)
+	{
+		return (1);
+	}
 	if (c >= 65 && c <= 90)
 	{
 		return (0);
@@ -15,10 +19,6 @@ int _islower(int c)
 	if (c >= 48 && c <= 57)
 	{
 		return (0);
-	}
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
 	}
 	return (0);
 }
