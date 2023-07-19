ebd54ba4067800f789bbb2c04dc1cd3c1982a927
padwalker <dawitgrmay@gmail.com>
2023-07-19 20:27:03 +0000

    A function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				}
 				if (i * j >= 10)
 				{
-				        printf("%d, ", j * i);
+					printf("%d, ", j * i);
 				}
 			}
 			if (j == 9)
@@ -32,11 +32,10 @@ void times_table(void)
 				}
 				if (j * i >= 10)
 				{
-				        printf("%d ", j * i);
+					printf("%d ", j * i);
 				}
 			}
 		}
 		printf("\n");
 	}
-	printf("\n");
 }
