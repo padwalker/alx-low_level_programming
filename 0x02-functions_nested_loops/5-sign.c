6343d21454ddf32a65ed3d2fe4554f5350eaa3c8
padwalker <dawitgrmay@gmail.com>
2023-07-19 00:44:23 +0000

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,25 +2,27 @@
 
 /**
  * print_sign - function checks numbers and determin +ve, zero or -ve
+ * num - argument passed for checking
  *
  * Returns: 1 if number is positive
  * | 0 if number is zero
  * |-1 if number is negative
+ *
  */
 
-int print_sign(int n)
+int print_sign(int num)
 {
-	if (n > 0)
+	if (num > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	if (n == 0)
+	if (num == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	if (n < 0)
+	if (num < 0)
 	{
 		_putchar('-');
 		return (-1);
