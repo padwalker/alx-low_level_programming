0ac51a0a73f3a130c06204e8e0f41d39d2192156
padwalker <dawitgrmay@gmail.com>
2023-07-19 00:51:36 +0000

    compute absolute value of num

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,12 @@
 
 /**
  * _abs - computes absolute value of an integer num.
- * num: the number to be checked
+ * num - the number to be checked
  *
- * Return: if num is +ve return num, 
+ * Return: if num is +ve return num,
  * if 0 return 0.
- * if num is -ve return -num. 
+ * if num is -ve return -num.
  * 0 otherwise
- *
  */
 int _abs(int num)
 {
